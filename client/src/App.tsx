bcc7a79313d4022ca5125a54a4f8ba1015272b64
RM3 <roger@studioavante.com>
2025-12-21 18:35:06 -0300

    client update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { Flex, Text, Button, Grid, Box, Inset, Card, AspectRatio, Link, Heading, Spinner } from '@radix-ui/themes'
+import { Flex, Text, Button, Grid, Box, Inset, Card, AspectRatio, Link, Heading, Spinner, Badge, Separator } from '@radix-ui/themes'
 import { useAccount, useBlockNumber } from '@starknet-react/core';
 import { useDojoSDK } from '@dojoengine/sdk/react';
 import { WalletAccount } from './wallet-account';
@@ -43,7 +43,12 @@ export default function App() {
   //   }
   // }, [sdk]);
 
+  const [contractMode, setContractMode] = useState(true);
   const [tokenId, setTokenId] = useState(0);
+  useEffect(() => {
+    setContractMode(tokenId == 0);
+  }, [tokenId]);
+
   const items = useMemo(() => {
     return new Array(totalSupply).fill(0).map((_, index) => <TokenImage key={index} tokenId={index + 1} setTokenId={setTokenId} />);
   }, [totalSupply]);
@@ -58,11 +63,17 @@ export default function App() {
       </Grid>
       <Box width='50%'>
         <WalletAccount />
+        <Separator size="4" my="3" />
         <Text>
           block [{blockNumber}] minted [{totalSupply}]
         </Text>
-        {tokenId && <TokenImage tokenId={tokenId} />}
-        {!tokenId && <ContractInfo />}
+        <Separator size="4" my="3" />
+        <Flex direction='row' gap='2' m='2'>
+          <Button variant={contractMode ? 'solid' : 'soft'} onClick={() => setContractMode(true)}>Contract</Button>
+          <Button variant={!contractMode ? 'solid' : 'soft'} onClick={() => setContractMode(false)} disabled={!tokenId}>Token #{tokenId}</Button>
+        </Flex>
+        {contractMode && <ContractInfo />}
+        {!contractMode && <TokenInfo tokenId={tokenId} />}
       </Box>
     </Flex>
   )
@@ -72,27 +83,99 @@ export default function App() {
 
 
 //-----------------------------------
-// Token #XX Image
+// Contract metadata
 //
 const ContractInfo = () => {
   const { client } = useDojoSDK<() => any, SchemaType>();
-  const [uri, setUri] = useState<any>({});
+  const [uri, setUri] = useState<string>();
   useEffect(() => {
     client?.token.contractUri().then((uri: string) => {
-      const json = JSON.parse(uri.replace('data:application/json,', ''));
-      setUri(json);
+      setUri(uri);
     });
   }, [client]);
+  const { json, isJsonValid, isUriValid } = useUri(uri);
   return (
     <Box>
-      <Heading>{uri.name} / {uri.symbol}</Heading>
-      <Text>{uri.description}</Text>
-      <Link href={uri.external_link} target='_blank'>{uri.external_link}</Link>
+      <UriStatus isUriValid={isUriValid} isJsonValid={isJsonValid} />
+      <Text>symbol: [{json?.symbol}]</Text><br />
+      <Text>name: [{json?.name}]</Text><br />
+      <Text>description: [{json?.description}]</Text><br />
+      <Text>external_link: [<Link href={json?.external_link} target='_blank'>{json?.external_link}</Link>]</Text><br />
+
+      <Metadata json={json} />
+
       <Flex direction='row' gap='2'>
-        <Image src={uri.image} label={`IMAGE`} />
-        <Image src={uri.banner_image} label={`BANNER`} />
+        <Box style={{ width: '50%' }}>
+          <Image src={json?.image} label={`IMAGE`} />
+        </Box>
+        <Box style={{ width: '50%' }}>
+          <Image src={json?.banner_image} label={`BANNER`} />
+        </Box>
       </Flex>
     </Box>
+  );
+};
+
+const useUri = (uri: string | undefined) => {
+  const [isUriValid, setIsUriValid] = useState<boolean | undefined>(false);
+  const [isJsonValid, setIsJsonValid] = useState<boolean | undefined>(false);
+  const [json, setJson] = useState<any | undefined>(undefined);
+  useEffect(() => {
+    setIsUriValid(undefined);
+    setIsJsonValid(undefined);
+    setJson(undefined);
+    if (uri) {
+      try {
+        const url = new URL(uri);
+        setIsUriValid(true);
+      } catch (error) {
+        console.error(`INVALID URL:`, uri, error);
+        setIsUriValid(false);
+      }
+      try {
+        const json = JSON.parse(uri.replace('data:application/json,', ''));
+        setJson(json);
+        setIsJsonValid(true);
+      } catch (error) {
+        console.error(`INVALID JSON:`, uri, error);
+        setIsJsonValid(false);
+      }
+    }
+  }, [uri]);
+  return {
+    uri,
+    isUriValid,
+    isJsonValid,
+    json,
+  }
+};
+
+const UriStatus = ({
+  isUriValid,
+  isJsonValid,
+}: {
+  isUriValid: boolean | undefined,
+  isJsonValid: boolean | undefined,
+}) => {
+  return (
+    <Flex direction='row' gap='2'>
+      <Badge color={isUriValid === undefined ? 'orange' : isUriValid === true ? 'green' : 'red'}>uri</Badge>
+      <Badge color={isJsonValid === undefined ? 'orange' : isJsonValid === true ? 'green' : 'red'}>json</Badge>
+    </Flex>
+  );
+};
+
+const Metadata = ({
+  json,
+}: {
+  json: any,
+}) => {
+  return (
+    <Text size='1'>
+      <pre style={{ overflowX: 'auto', backgroundColor: 'var(--gray-5)' }}>
+        {JSON.stringify(json, null, 2)}
+      </pre>
+    </Text>
   );
 };
 
@@ -101,6 +184,31 @@ const ContractInfo = () => {
 //-----------------------------------
 // Token #XX Image
 //
+const TokenInfo = ({
+  tokenId,
+}: {
+  tokenId: number,
+}) => {
+  const { client } = useDojoSDK<() => any, SchemaType>();
+  const [uri, setUri] = useState<string>();
+  useEffect(() => {
+    client?.token.tokenUri(tokenId).then((uri: string) => {
+      setUri(uri);
+    });
+  }, [client, tokenId]);
+  const { json, isJsonValid, isUriValid } = useUri(uri);
+  if (!json) return <Spinner />;
+  return (
+    <Box>
+      <UriStatus isUriValid={isUriValid} isJsonValid={isJsonValid} />
+      <Text>name: [{json?.name}]</Text><br />
+      <Text>description: [{json?.description}]</Text><br />
+      <Image src={json?.image} label={json?.name} tokenId={tokenId} />
+      <Metadata json={json} />
+    </Box>
+  );
+};
+
 const TokenImage = ({
   tokenId,
   setTokenId,
@@ -109,18 +217,21 @@ const TokenImage = ({
   setTokenId?: (tokenId: number) => void
 }) => {
   const { client } = useDojoSDK<() => any, SchemaType>();
-  const [image, setImage] = useState('');
+  const [uri, setUri] = useState<string>();
   useEffect(() => {
     client?.token.tokenUri(tokenId).then((uri: string) => {
-      const json = JSON.parse(uri.replace('data:application/json,', ''));
-      setImage(json.image);
+      setUri(uri);
     });
   }, [client, tokenId]);
-  if (!image) return <Spinner />;
+  const { json, isJsonValid, isUriValid } = useUri(uri);
+  if (!json) return <Spinner />;
   return (
-    <Image src={image} label={`ASTRAEA#${tokenId}`} tokenId={tokenId} setTokenId={setTokenId} />
+    <Image src={json?.image} label={json?.name} tokenId={tokenId} setTokenId={setTokenId}>
+      <UriStatus isUriValid={isUriValid} isJsonValid={isJsonValid} />
+    </Image>
   );
 };
+
 
 //-----------------------------------
 // Generic Image
@@ -130,11 +241,17 @@ const Image = ({
   label,
   tokenId,
   setTokenId,
+  width = 'auto',
+  height = '100%',
+  children,
 }: {
   src: string,
   label: string,
   tokenId?: number,
   setTokenId?: (tokenId: number) => void
+  width?: string,
+  height?: string,
+  children?: React.ReactNode,
 }) => {
   return (
     <Box>
@@ -142,8 +259,8 @@ const Image = ({
         <AspectRatio ratio={1}>
           <img src={src}
             style={{
-              height: "100%",
-              width: 'auto',
+              width,
+              height,
               backgroundColor: "var(--gray-5)",
               cursor: setTokenId ? "pointer" : "default",
             }}
@@ -151,6 +268,7 @@ const Image = ({
           />
         </AspectRatio>
         <Text size='1' color='gray'>{label}</Text>
+        {children}
       </Flex>
     </Box>
   );
